91725e776518002d9d712dd31c111b356850977c
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2025-03-30 20:12:34 +0300

    new script to get component meta data

--- buridan_ui/static/scripts.py
+++ buridan_ui/static/scripts.py
@@ -1,5 +1,9 @@
 import os
 
+from buridan_ui.config import LOCAL_BASE_CHART_PATH, LOCAL_BASE_PANTRY_PATH
+from buridan_ui.export import config
+from buridan_ui.wrappers.base.utils.meta import get_file_times
+
 
 def count_python_files_in_folder(folder_name) -> int:
     """Get the total number of files in a folder"""
@@ -9,3 +13,25 @@ def count_python_files_in_folder(folder_name) -> int:
         total_files += len([f for f in filenames if f.endswith(".py")])
 
     return total_files
+
+
+def get_directory_meta_data():
+    charts, pantry = {}, {}
+
+    # Get the chart directories metadata
+    for directory in config.CHARTS.keys():
+        full_path = os.path.join(LOCAL_BASE_CHART_PATH, directory, "")
+        charts[directory] = get_file_times(full_path)
+
+    # Get the pantry directories metadata
+    for directory in config.COMPONENTS.keys():
+        full_path = os.path.join(LOCAL_BASE_PANTRY_PATH, directory, "")
+        pantry[directory] = get_file_times(full_path)
+
+    # Write the metadata to static/meta.py
+    with open("meta.py", "w") as file:
+        file.write(f"""ChartMetaData = {charts}\nPantryMetaData = {pantry}""")
+
+
+if __name__ == "__main__":
+    get_directory_meta_data()
